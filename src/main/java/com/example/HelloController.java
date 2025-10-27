c48073863ddbc06a02435699feeec4ad61a1dffe
danne <xsz300@gmail.com>
2025-10-27 15:02:19 +0100

    test gui med dateAndTime, StringProperty model.

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -12,11 +21,40 @@ public class HelloController {
 
     @FXML
     private Label messageLabel;
+    public Label dateAndTimeLable;
+    public Button upDateButton;
+    public VBox smallVboxContent;
+    private double rotation = 0;
+
+    private Timeline timeline;
+
+    public HelloModel getModel(){
+        return model;
+    }
 
     @FXML
     private void initialize() {
+        dateAndTimeLable.textProperty().bind(model.dateTimeProperty());
+        //A[ dateAndTimeLable.textProperty() ].bind(B[ model.dateTimeProperty() ])
         if (messageLabel != null) {
             messageLabel.setText(model.getGreeting());
         }
+//        if(dateAndTimeLable != null) {
+//            dateAndTimeLable.setText(LocalDateTime.now()
+//                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+//        }
+        timeline = new Timeline(new KeyFrame(Duration.seconds(2), _ ->{
+            model.setDateTime(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public void updateButtonAction(ActionEvent actionEvent) {
+        model.setDateTime(LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        rotation += 27;
+        smallVboxContent.setRotate(rotation);
     }
 }
--- src/main/java/com/example/HelloFX.java
+++ src/main/java/com/example/HelloFX.java
@@ -4,18 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloFX.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 640, 480);
         stage.setTitle("Hello MVC");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
