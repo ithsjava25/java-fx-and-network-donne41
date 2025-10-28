bef25168859f1e99e6b812b459f96188d6940d40
danne <xsz300@gmail.com>
2025-10-28 09:38:34 +0100

    start of halloweenGame

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,9 +1,21 @@
 package com.example;
 
+import javafx.scene.image.*;
+
 /**
  * Model layer: encapsulates application data and business logic.
  */
 public class HelloModel {
+
+    Image noSmash;
+    Image smash1;
+    Image smash2;
+    Image smash3;
+    Image smash4;
+
+    public HelloModel(){
+        noSmash = new Image(getClass().getResource("/lantern.png").toExternalForm());
+    }
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
