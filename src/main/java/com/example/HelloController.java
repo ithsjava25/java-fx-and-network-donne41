5f09161d638c181dcdcc4897a13b2dd6128b1739
danne <xsz300@gmail.com>
2025-10-28 11:46:54 +0100

    implementet smash method, set backgorund and point system.

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -11,19 +17,48 @@ public class HelloController {
     private final HelloModel model = new HelloModel();
 
 
-    public HelloModel getModel(){
+    public HelloModel getModel() {
         return model;
     }
 
     @FXML
     private Label messageLabel;
+    Timeline timeline;
 
     @FXML
     private void initialize() {
-        if (messageLabel != null) {
-            messageLabel.setText(model.getGreeting());
-        }
+        messageLabel.textProperty().bind(
+                Bindings.concat(model.pointsProperty().asString(), " Poäng")
+        );
+        startNextEvent();
+    }
 
+    private void startNextEvent() {
+        new Timeline(
+                new KeyFrame(Duration.millis(3000 * Math.random()),
+                        event -> {
+                            model.changeImage(
+                                    (int) (3 * Math.random()),
+                                    (int) (3 * Math.random()));
+                            startNextEvent();
+                        }
+                )).play();
+    }
+
+    public void image1(MouseEvent mouseEvent) {
+//       if( mouseEvent.getSource() instanceof ImageView view){
+//           view.getImage();
+//       }
+        model.smash(0);
+    }
+
+    public void image2(MouseEvent mouseEvent) {
+        model.smash(1);
+
+    }
+
+    public void image3(MouseEvent mouseEvent) {
+        model.smash(2);
 
     }
 }
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.*;
 
 /**
@@ -13,12 +17,71 @@ public class HelloModel {
     Image smash3;
     Image smash4;
 
+    ObjectProperty<Image> left;
+    ObjectProperty<Image> middle;
+    ObjectProperty<Image> right;
+
+    private IntegerProperty points;
+
     public HelloModel(){
         noSmash = new Image(getClass().getResource("/lantern.png").toExternalForm());
         smash1 = new Image(getClass().getResource("/lantern1.png").toExternalForm());
         smash2 = new Image(getClass().getResource("/lantern2.png").toExternalForm());
         smash3 = new Image(getClass().getResource("/lantern3.png").toExternalForm());
         smash4 = new Image(getClass().getResource("/lantern4.png").toExternalForm());
+        left = new SimpleObjectProperty<Image>(noSmash);
+        middle = new SimpleObjectProperty<Image>(noSmash);
+        right = new SimpleObjectProperty<Image>(noSmash);
+
+        points = new SimpleIntegerProperty(0);
+    }
+
+    public Image getLeft() {
+        return left.get();
+    }
+
+    public ObjectProperty<Image> leftProperty() {
+        return left;
+    }
+
+    public void setLeft(Image left) {
+        this.left.set(left);
+    }
+
+    public Image getMiddle() {
+        return middle.get();
+    }
+
+    public ObjectProperty<Image> middleProperty() {
+        return middle;
+    }
+
+    public void setMiddle(Image middle) {
+        this.middle.set(middle);
+    }
+
+    public Image getRight() {
+        return right.get();
+    }
+
+    public ObjectProperty<Image> rightProperty() {
+        return right;
+    }
+
+    public void setRight(Image right) {
+        this.right.set(right);
+    }
+
+    public int getPoints() {
+        return points.get();
+    }
+
+    public IntegerProperty pointsProperty() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points.set(points);
     }
 
     public Image getNoSmash(){
@@ -30,6 +93,38 @@ public class HelloModel {
     public String getGreeting() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
+        return "Welcome to halloweenSmashGame " + javafxVersion + ", running on Java " + javaVersion + ".";
+    }
+
+    public void smash(int lantern) {
+    //if model is with face, get point and return to no face model.
+        if(lantern == 0 && getLeft() != noSmash){
+            setPoints(getPoints()+1);
+            setLeft(noSmash);
+        }if(lantern == 1 && getMiddle() != noSmash){
+            setPoints(getPoints()+1);
+            setMiddle(noSmash);
+        }if(lantern == 2 && getRight() != noSmash){
+            setPoints(getPoints()+1);
+            setRight(noSmash);
+        }
+    }
+
+    public void changeImage(int lantern, int imageId){
+        switch(lantern){
+            case 0 -> setLeft( getImage(imageId));
+            case 1 -> setMiddle( getImage(imageId));
+            case 2 -> setRight( getImage(imageId));
+        }
+    }
+
+    private Image getImage(int imageId) {
+        return switch(imageId){
+            case 0 -> smash1;
+            case 1 -> smash2;
+            case 2 -> smash3;
+            case 3 -> smash4;
+            default -> noSmash;
+        };
     }
 }
