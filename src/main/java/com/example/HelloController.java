07ef683b6e2c91b90403ea0eb7fee2dade14a426
danne <xsz300@gmail.com>
2025-10-29 21:06:09 +0100

    new message as new label.

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,6 +3,11 @@ package com.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -13,15 +18,27 @@ public class HelloController {
 
     @FXML
     private Label messageLabel;
+    private Label messageSent;
+
+
+    @FXML
+    private ImageView backgroundImage;
+    @FXML
+    private VBox messageList;
+    @FXML
+    private TextField outgoingMessage;
 
     @FXML
     private void initialize() {
+
         if (messageLabel != null) {
             messageLabel.setText(model.getGreeting());
         }
     }
 
     public void sendButtonCliked(ActionEvent actionEvent) {
-
+        Label message = new Label(outgoingMessage.getText());
+        messageList.getChildren().add(message);
+        outgoingMessage.clear();
     }
 }
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,9 +1,26 @@
 package com.example;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Label;
+import javafx.scene.image.*;
+
 /**
  * Model layer: encapsulates application data and business logic.
  */
 public class HelloModel {
+
+    Image background;
+    Label message;
+
+    ObjectProperty<Label> messageProp; //might do send and recived
+
+    public HelloModel(){
+        messageProp = new SimpleObjectProperty<Label>();
+
+    }
+
+
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
